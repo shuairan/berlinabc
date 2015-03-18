20d67c7233e749fc2c217a01904ebf8fb729c4a6
Shuairan <S.Riedinger+github@gmail.com>
2015-03-18 22:33:56 +0100

    refactor: add wikipedia śtuff to wikicrawler

--- src/generate_data.py
+++ src/generate_data.py
@@ -1,50 +1,16 @@
 import json
-import itertools
 from pprint import pprint
-import re
 
 from fazcrawler import FazFeedCrawler#, FazHttpCrawler
-import wikicrawler
+from wikicrawler import WikiStationCrawler
 
-def get_station_match(candidates):
-    print(candidates)
-    for candidate in candidates:
-        matches = list(filter(lambda s: candidate in s, stations))
-        if matches:
-            print("Candidate %s has match %s" % (candidate, matches))
-            return matches[0]
-    return None
-
-def find_station(article):
-    candidates = []
-    
-    candidates.append(article["title"])
-    alphanumeric = re.sub('[^0-9a-zA-Zäöüß]+', ' ', article["title"])
-    candidates.append(alphanumeric)
-    candidates.extend(alphanumeric.split(" "))
-    candidates.extend(article["tags"])
-    candidates = [c for c in candidates if len(c) > 5]
-    station = get_station_match(candidates)
-    
-    if station is None:
-        print("no station for %s (%s)" % (article["title"], candidates))
-    return station
-
-
-def get_coordinates(station):
-    coords = wikicrawler.get_coordinates(station)
-    print("Coords", coords) 
-    return coords
-    #print("Find coords for %s (%s)" % (article["title"], article["link"]))
-    #print(list(matches))
-    #for station in stations:    
+wikicat = [ 'Kategorie:Bahnhof_der_S-Bahn_Berlin', 'Kategorie:U-Bahnhof_in_Berlin' ]   
 
 def main():
     # get list of stations from Wikipedia:
     print("Crawlink Wikipedia for stations")
-    wikicat = [ 'Kategorie:Bahnhof_der_S-Bahn_Berlin', 'Kategorie:U-Bahnhof_in_Berlin' ]
-    stations = list(itertools.chain(* [wikicrawler.get_page_list(s) for s in wikicat]))
-
+    wiki = WikiStationCrawler(wikicat)
+    
     # get all blog article links from FAZ:
     print("Crawling blog.faz.net for article links")
     #faz_http = FazHttpCrawler('berlinabc')
@@ -71,10 +37,11 @@ def main():
         entry['tags'] = [tag['term'] for tag in rss_data.tags]
         entry['author'] = rss_data.author
         entry['published'] = rss_data.published
+        
         #pprint(entry)
-        station = find_station(entry)
+        station = wiki.find_station(entry)
         if station:
-            coords = get_coordinates(station)
+            coords = wiki.get_coordinates(station)
             entry['coordinates'] = coords
 
         articles.append(entry)
--- src/wikicrawler.py
+++ src/wikicrawler.py
@@ -1,46 +1,78 @@
 import urllib.request
 import urllib.parse
 import json
+import itertools
+import re
+
 
 WIKIPEDIA_API="https://de.wikipedia.org/w/api.php"
-
-
-def wiki_request(params):
-    data = urllib.parse.urlencode(params)
-    #print(WIKIPEDIA_API + '?' + data)
-    data = data.encode('utf-8')
-    request = urllib.request.Request(WIKIPEDIA_API)
     
-    f = urllib.request.urlopen(request, data)
-    str_response = f.read().decode('utf-8')
-    return json.loads(str_response)
+class WikiCrawler():
+    def get_pages_from_category(self, category):
+        """ http://de.wikipedia.org/w/api.php?action=query&list=categorymembers&cmtype=page&cmlimit=500&cmtitle=CATEGORY """
+        params = {
+            'action': 'query',
+            'list': 'categorymembers',
+            'format' : 'json',
+            'cmtype' : 'page',
+            'cmlimit': 500,
+            'continue' : '',
+            'cmtitle' : category
+        }
+        json = self.wiki_request(params)
+        return [page["title"] for page in json["query"]["categorymembers"]]
 
-def get_coordinates(title):
-    """ https://de.wikipedia.org/w/api.php?action=query&prop=coordinates&titles=TITEL """
-    params = {
-        'action': 'query',
-        'prop': 'coordinates',
-        'format' : 'json',
-        'continue' : '',
-        'titles': title
-    }
-    json = wiki_request(params)
-    page = json['query']['pages']
-    return [(c[0]['lat'], c[0]['lon']) for c in [page[key]['coordinates'] for key in page]][0]
+    def get_coordinates(self, title):
+        """ https://de.wikipedia.org/w/api.php?action=query&prop=coordinates&titles=TITEL """
+        params = {
+            'action': 'query',
+            'prop': 'coordinates',
+            'format' : 'json',
+            'continue' : '',
+            'titles': title
+        }
+        json = self.wiki_request(params)
+        page = json['query']['pages']
+        return [(c[0]['lat'], c[0]['lon']) for c in [page[key]['coordinates'] for key in page]][0]
 
-def get_pages_from_category(category):
-    """ http://de.wikipedia.org/w/api.php?action=query&list=categorymembers&cmtype=page&cmlimit=500&cmtitle=CATEGORY """
-    params = {
-        'action': 'query',
-        'list': 'categorymembers',
-        'format' : 'json',
-        'cmtype' : 'page',
-        'cmlimit': 500,
-        'continue' : '',
-        'cmtitle' : category
-    }
-    return wiki_request(params)
 
-def get_page_list(category):
-    json = get_pages_from_category(category)
-    return [page["title"] for page in json["query"]["categorymembers"]]
+    def wiki_request(self, params):
+        data = urllib.parse.urlencode(params)
+        #print(WIKIPEDIA_API + '?' + data)
+        data = data.encode('utf-8')
+        request = urllib.request.Request(WIKIPEDIA_API)
+        
+        f = urllib.request.urlopen(request, data)
+        str_response = f.read().decode('utf-8')
+        return json.loads(str_response)
+
+class WikiStationCrawler(WikiCrawler):
+    def __init__(self, categories):
+        self.categories = categories
+        self.stations = self.get_station_list(categories)
+        
+    def get_station_list(self, categories):
+        return list(itertools.chain(* [self.get_pages_from_category(s) for s in categories]))
+
+    def get_station_match(self, candidates):
+        print(candidates)
+        for candidate in candidates:
+            matches = list(filter(lambda s: candidate in s, self.stations))
+            if matches:
+                print("Candidate %s has match %s" % (candidate, matches))
+                return matches[0]
+        return None
+
+    def find_station(self, article):
+        candidates = []
+        candidates.append(article["title"])
+        alphanumeric = re.sub('[^0-9a-zA-Zäöüß]+', ' ', article["title"])
+        candidates.append(alphanumeric)
+        candidates.extend(alphanumeric.split(" "))
+        candidates.extend(article["tags"])
+        candidates = [c for c in candidates if len(c) > 5]
+        station = self.get_station_match(candidates)
+        
+        if station is None:
+            print("no station for %s (%s)" % (article["title"], candidates))
+        return station
